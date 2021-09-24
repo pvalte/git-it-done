3612fcd5d79cb243bf870319bd021bd79c89d4a7
Paige A <pvaltemare@gmail.com>
2021-09-23 19:30:27 -0700

    add language to search

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var formSubmitHandler = function (event) {
     event.preventDefault();
@@ -40,6 +41,20 @@ var getUserRepos = function (user) {
 };
 
 userFormEl.addEventListener("submit", formSubmitHandler);
+
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+  
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+          alert('Error: GitHub User Not Found');
+        }
+    });
+};
 
 var displayRepos = function (repos, searchTerm) {
     // check if api returned any repos
@@ -87,3 +102,16 @@ var displayRepos = function (repos, searchTerm) {
         repoContainerEl.appendChild(repoEl);
     }
 };
+
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+    if(language){
+        getFeaturedRepos(language);
+
+        // clear old content
+        repoContainerEl.textContent = "";
+    }
+
+};
+
+languageButtonsEl.addEventListener("click", buttonClickHandler);
